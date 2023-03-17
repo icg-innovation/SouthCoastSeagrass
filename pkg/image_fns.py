c33551272d6e042c6fe756132bba488911ca83b6
olliekemp <103338687+olliekemp@users.noreply.github.com>
2023-03-17 14:56:46 +0000

    Added water bitmask func and cloud percentage
    Added function to take the QA_band bit mask and find the pixels with/without water and to calculate the total cloud coverage percentage

--- pkg/image_fns.py
+++ pkg/image_fns.py
@@ -60,6 +60,49 @@ def QA_cloud_perc(file_list, QA_band):
     return water_count
 
 def water_from_bitmask(bitmask):
-    
-    
-    return
+    '''
+    Converts a earth engine QA bitmask to a boolean array of water pixels.
+    Bitmask array conversion from https://stackoverflow.com/questions/22227595/convert-integer-to-binary-array-with-suitable-padding
+    Arguments:
+    bitmask:np.ndarray, shape(pix_x, pix_y)
+
+    returns:
+    water_bitmask : np.ndarray, shape(pix_x, pix_y). Boolean array of pixels where water is present 
+    '''
+
+    # number of bits to convert bitmask to 
+    m = 16 
+    # Function to convert an integer to a string binary representation
+    to_str_func = np.vectorize(lambda x: np.binary_repr(x).zfill(m))
+    # Calculte binary representations
+    strs = to_str_func(bitmask)
+    # Create empty array for the bitmask
+    bitmask_bits = np.zeros(list(bitmask.shape) + [m], dtype=np.int8)
+    # Iterate over all m  bits
+    for bit_ix in range(0, m):
+        # Get the bits
+        fetch_bit_func = np.vectorize(lambda x: x[bit_ix] == '1')
+        # Store the bits
+        bitmask_bits[:, :, bit_ix] = fetch_bit_func(strs).astype("int8")
+
+    # The water bitmask is stored in bit 7 (index 15-7=8).
+    water_bitmask = bitmask_bits[:, :, 8] == 0
+
+    return water_bitmask
+
+def calculate_cloud_percentage(water_counts):
+    '''
+    Calculates the percentage of water covered by clouds for a given array
+    of sums of water pixels. Assumes the maximum water count corresponds to the 
+    clearest, least cloudy day and calculates the percentages from there. 
+
+    Arguments:
+    water_counts : np.ndarray, shape (n_files,), counts of all water pixels for a given image
+
+    Returns:
+    cover_percents : np.ndarray, shape (n_files), percentage of cloud cover for a given image
+
+    '''
+    cover_percents = 100*(water_counts / water_counts.max())
+
+    return cover_percents
